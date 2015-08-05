c8eafce1527c92790198e6389b14fab525620b42
SodaDev <soda@Marcins-MacBook-Pro.local>
2015-08-05 10:32:29 +0200

    Added loading indicator

--- EliteScheduleApp/www/app/services/eliteApi.js
+++ EliteScheduleApp/www/app/services/eliteApi.js
@@ -2,30 +2,36 @@
  * Created by soda on 05/08/15.
  */
 angular.module('eliteApp')
-    .factory('eliteApi', ['$http', '$q', function($http, $q){
+    .factory('eliteApi', ['$http', '$q', '$ionicLoading', function($http, $q, $ionicLoading){
         var currentLeagueId;
         return {
             getLeagues: function(callback){
                 var deferred = $q.defer();
+                $ionicLoading.show({ template: 'Loading...'});
                 $http.get("http://elite-schedule.net/api/leaguedata/")
                     .success(function(data){
+                        $ionicLoading.hide();
                         deferred.resolve(data);
                     })
                     .error(function(){
-                        console.log("Error while making leagues HTTP call.");
+                        console.error("Error while making leagues HTTP call.");
+                        $ionicLoading.hide();
                         deferred.reject();
                     });
                 return deferred.promise;
             },
             getLeagueData: function(callback){
                 var deferred = $q.defer();
+                $ionicLoading.show({ template: 'Loading...'});
                 $http.get("http://elite-schedule.net/api/leaguedata/" + currentLeagueId)
                     .success(function(data, status){
-                        console.log('Received schedule data via HTTP.', data, status)
+                        console.error('Received schedule data via HTTP.', data, status);
+                        $ionicLoading.hide();
                         deferred.resolve(data);
                     })
                     .error(function(){
-                        console.log("Error while making league data HTTP call for id: " + currentLeagueId);
+                        console.error("Error while making league data HTTP call for id: " + currentLeagueId);
+                        $ionicLoading.hide();
                         deferred.reject();
                     });
                 return deferred.promise;
